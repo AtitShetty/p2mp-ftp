469d95a844abe10643286f688f08f1947d86a0b6
Rishi <rjain9@ncsu.edu>
2017-11-25 13:24:09 -0500

    send_ack()

--- server/src/main/java/ip_p2mp_ftp/server/Server.java
+++ server/src/main/java/ip_p2mp_ftp/server/Server.java
@@ -1,13 +1,19 @@
 package ip_p2mp_ftp.server;
 
+import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
+import java.nio.ByteBuffer;
 
 public class Server extends Thread {
 	private DatagramSocket socket;
-
+	private InetAddress address;
+	private int port;
+	private static double p;
+	private String sequence;
+		
 	public Server() throws SocketException {
 		socket = new DatagramSocket(7735);
 	}
@@ -18,8 +24,8 @@ public class Server extends Thread {
 				DatagramPacket packet = new DatagramPacket(new byte[256], 256);
 				this.socket.receive(packet);
 
-				InetAddress address = packet.getAddress();
-				int port = packet.getPort();
+				address = packet.getAddress();
+				port = packet.getPort();
 				String request = new String(packet.getData(), 0, packet.getLength());
 				System.out.println(request);
 				String response = "Received:" + request;
@@ -29,4 +35,17 @@ public class Server extends Thread {
 			System.out.println(e.getMessage());
 		}
 	}
+	
+
+	 private void send_ack(int s) throws IOException
+	 {
+		 try{
+	    	int ack = Integer.parseInt(sequence+"00000000000000001010101010101010", 2);
+	    	byte[] send_ack = ByteBuffer.allocate(8).putInt(ack).array();
+	    	DatagramPacket sendack = new DatagramPacket(send_ack, send_ack.length, address, port);
+	    	socket.send(sendack);
+		 }catch (Exception e) {
+				System.out.println(e.getMessage());
+		}
+	 } 
 }
