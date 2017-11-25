7b53a7f33945f88baa10b52b7b0260f547be448c
atit-shetty <atit.shetty@hcl.com>
2017-11-25 17:22:05 -0500

    mods

--- client/src/main/java/p2mp/client/Main.java
+++ client/src/main/java/p2mp/client/Main.java
@@ -1,6 +1,5 @@
 package p2mp.client;
 
-import java.io.IOException;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
@@ -10,24 +9,32 @@ import java.util.Set;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException, InterruptedException {
+	public static void main(String[] args) throws Exception {
+
+		int argsSize = args.length;
+
+		if (argsSize < 4) {
+			throw new Exception("Check input format");
+		}
+
 		Set<String> servers = new HashSet<String>();
-		int port = 0;
-		String fileName = null;
-		int mss = 500;
+		int port = Integer.parseInt(args[argsSize - 3]);
+		String fileName = args[argsSize - 2];
+		int mss = Integer.parseInt(args[argsSize - 1]);
+		System.out.println(Arrays.toString(args));
 		
-		for (int i = 0; i < args.length; i++) {
+		for (int i = argsSize - 4; i >= 0; i--) {
 			
 			try {
 				servers.add(InetAddress.getByName(args[i]).getHostAddress());
 			} catch (UnknownHostException e) {
-				System.out.println("File will be sent to following servers: " + Arrays.toString(servers.toArray()));
-				port = Integer.parseInt(args[i]);
-				fileName = args[i + 1];
-				mss = Integer.parseInt(args[i + 2]);
+				throw new Exception("Check Host addresses or input format");
 			}
 		}
 
+		// System.out.println("Port is: " + port);
+		// System.out.println("Filename is: " + fileName);
+		// System.out.println("mss is:" + mss);
 		DatagramSocket socket = new DatagramSocket();
 
 		Thread requestThread = new Thread(new SendFiles(socket, fileName, mss, servers, port, 5000));
